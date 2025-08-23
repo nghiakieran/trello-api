831721cca43232c5ab68b501a8ff48ce94ae3c08
nghiakieran <lechinghia202@gmail.com>
2025-08-23 15:47:23 +0700

    Config git

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const hostName = 'localhost'
 const port = 8017
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('<h1>Hello World</h1>')
 })
 
 app.listen(port, hostName, () => {
