2a8f3170f15dc51e3106431adf1ba4de136f0ab7
nghiakieran <lechinghia202@gmail.com>
2025-08-24 12:45:32 +0700

    Add connect mongodb and config env variables

--- src/server.js
+++ src/server.js
@@ -1,26 +1,39 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CLOSE_DB, CONNECT_DB } from './config/mongodb'
+import { env } from './config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = env.APP_HOST
+  const port = env.APP_PORT
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${hostname}:${port}/`)
+  })
+
+  // CLEANUP trước khi tắt server
+  exitHook(() => {
+    CLOSE_DB()
+  })
+}
+
+// IIFE:
+(async () => {
+  try {
+    console.log('Connecting to MongoDB Cloud Atlas...')
+    await CONNECT_DB()
+    console.log('Connected to MongoDB Cloud Atlas...')
+
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
