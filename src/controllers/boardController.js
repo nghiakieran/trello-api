6a59e6c074b4935ef1ae70303cc06244e4116252
nghiakieran <lechinghia202@gmail.com>
2025-08-30 12:37:42 +0700

    Add board service

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,8 +1,10 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
-    res.status(StatusCodes.CREATED).json({ message: 'Created board' })
+    const createdBoard = await boardService.createNew(req.body)
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) {
     next(error)
   }
